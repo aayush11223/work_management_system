600b970cfabc5c32b3869de6078ddc7b312402c6
Aayush Basnet <aayush9823@gmail.com>
2026-06-09 11:44:32 +0545

    Cancel color changed of form

--- src/components/leave/LeaveRequestForm.vue
+++ src/components/leave/LeaveRequestForm.vue
@@ -59,7 +59,7 @@
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+        <v-btn color="red darken-1" text @click="close">Cancel</v-btn>
         <v-btn color="blue darken-1" text @click="submit">Submit</v-btn>
       </v-card-actions>
     </v-card>
--- src/components/worklog/WorkLogForm.vue
+++ src/components/worklog/WorkLogForm.vue
@@ -55,7 +55,7 @@
 
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="handleCancel">Cancel</v-btn>
+        <v-btn color="red darken-1" text @click="handleCancel">Cancel</v-btn>
         <v-btn color="blue darken-1" text @click="handleSubmit">Submit</v-btn>
       </v-card-actions>
     </v-card>
